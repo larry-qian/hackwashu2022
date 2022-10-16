f6edcfbaa706198f6899e024e1da9f0575e1e98c
Skyturtl <112505166+Skyturtl@users.noreply.github.com>
2022-10-15 19:36:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-// This function inserts our autoSearch function into the page's code
 function insertScript() {
- // This selects the focused tab for the operation and passes the autoSearch function
- chrome.tabs.query({active: true, currentWindow: true}, tabs => {
- chrome.scripting.executeScript({target: {tabId: tabs[0].id}, function: mainFunct})
- })
- 
- // This closes the extension pop-up to select the website search bar
- window.close();
+	chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+	chrome.scripting.executeScript({target: {tabId: tabs[0].id}, function: mainFunct})
+	})
+	window.close();
 }
  
-// This is an event listener that detects clicks on our "Start Random Search" button
 document.getElementById('buttonTwo').addEventListener('click', insertScript)
  
-// This function selects a random topic from an array and 
-function mainFunct() {
- document.querySelector('[name="ctl00$Body$ctl15"]').click();
- 
-//  var searchBar = document.getElementByName("ctl00$Body$ctl15");
- 
-//  searchBar.value = searchBar.value + selection;
- 
-//  document.getElementById("searchform2").submit();
-}
 const times = [];
 const dates = [];
 const regsht = [];
@@ -53,4 +38,13 @@ function webScrapeWkSht() {
 	for(let i = 2; i< (document.querySelectorAll('#grvFirstChoices > tbody > tr').length+1); i++) {
 		regsht.push(document.querySelectorAll('#grvFirstChoices > tbody > tr:nth-child('+i+') > td:nth-child(4) > span')[0].textContent.trim());
 	}
+}
+
+
+function mainFunct() {
+	document.querySelector('[name="ctl00$Body$ctl15"]').click();
+	webScrapeCN();
+	webScrapeTime();
+	webScrapeDate();
+	webScrapeWkSht();
 }
