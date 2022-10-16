059c276d0cd5dbcda1ed04d3a5008163147fedba
Skyturtl <112505166+Skyturtl@users.noreply.github.com>
2022-10-16 02:50:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ class Course {
 
 function insertScript1() {
 	chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-	chrome.scripting.executeScript({target: {tabId: tabs[0].id}, function: mainFunct})
+	chrome.scripting.executeScript({target: {tabId: tabs[0].id}, function: getClasses})
 	})
 	window.close();
 }
 
 function insertScript2(){
 	chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-	chrome.scripting.executeScript({target: {tabId: tabs[0].id}, function: console.log('Generate')})
+	chrome.scripting.executeScript({target: {tabId: tabs[0].id}, function: mainFunct})
 	})
 	window.close();
 }
@@ -36,7 +36,7 @@ function webScrapeCN() {
 }
 
 function webScrapeTime() {
-	timeslist = [];
+	timelist = [];
 	numSections = 0;
 	for(let i = 0; i < (document.querySelectorAll('.MainTableRow').length); i++) {
 		if(!isNaN(document.querySelectorAll('.MainTableRow tbody tr td:nth-child(2)')[i*3].textContent)) {
@@ -49,7 +49,7 @@ function webScrapeTime() {
 }
 
 function webScrapeDate() {
-	dateslist = []
+	datelist = [];
 	for(let i = 0; i < (document.querySelectorAll('.MainTableRow').length); i++) {
 		if(!isNaN(document.querySelectorAll('.MainTableRow tbody tr td:nth-child(2)')[i*3].textContent)) {
 			dateslist.push(document.querySelectorAll('.MainTableRow tbody tr td:nth-child(3)')[i*2].textContent);
@@ -59,22 +59,24 @@ function webScrapeDate() {
 	}
 }
 
-function webScrapeWkSht() {
+function getClasses() {	
+	regsht = [];
+	numCourses = 0;
 	for(let i = 2; i< (document.querySelectorAll('#grvFirstChoices > tbody > tr').length+1); i++) {
 		regsht.push(document.querySelectorAll('#grvFirstChoices > tbody > tr:nth-child('+i+') > td:nth-child(4) > span')[0].textContent.trim());
 		numCourses++;
+		localStorage.setItem(''+(i-2), ''+regsht[i-2]);
+		console.log(regsht[i-2])
 	}
-	
+	return numCourses;
+
 }
 
-// funciton getClasses() {
-// 	console.log(webScrapeWkSht());
-// }
-
 function mainFunct() {
-	document.querySelector('[name="ctl00$Body$ctl15"]').click();
+	getClasses();
 	var searchBar = document.getElementById("Body_txtSearchKeyword");
-	for(let j = 0; j < numCourses; j++){
+	for(let i = 0; i < numCourses; i++){
+		console.log('hi')
 		searchBar.value = searchBar.value + regsht[i];
 		document.querySelector('[name="ctl00$Body$ctl15"]').click();
 		var name = regsht[i];
